3f825b0a804f84130c2055b655fb33edd91ca7f2
Silvio Sosio <silvio@sosio.it>
2026-06-07 12:12:26 +0200

    password utente

--- app/Filament/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -2,14 +2,18 @@
 
 namespace App\Filament\Resources\Users\Tables;
 
+use App\Models\User;
 use Filament\Actions\Action;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTable
 {
@@ -76,6 +80,33 @@ class UsersTable
                     ->icon(Heroicon::PaperAirplane)
                     ->color('primary')
                     ->action(fn ($record) => $record->sendAccess()),
+                Action::make('set_password')
+                    ->label('Imposta password')
+                    ->icon(Heroicon::LockClosed)
+                    ->color('warning')
+                    ->modalHeading('Imposta password')
+                    ->schema([
+                        TextInput::make('password')
+                            ->label('Nuova password')
+                            ->password()
+                            ->revealable()
+                            ->required()
+                            ->minLength(8),
+                        TextInput::make('password_confirmation')
+                            ->label('Conferma password')
+                            ->password()
+                            ->revealable()
+                            ->required()
+                            ->same('password'),
+                    ])
+                    ->action(function (array $data, User $record): void {
+                        $record->update(['password' => Hash::make($data['password'])]);
+
+                        Notification::make()
+                            ->title('Password aggiornata')
+                            ->success()
+                            ->send();
+                    }),
                 EditAction::make(),
             ])
             ->toolbarActions([
